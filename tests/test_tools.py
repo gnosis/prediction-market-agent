abddcd5fe4936514ea76e9873548d14091b4684d
Peter Jung <peter@jung.ninja>
2024-01-25 12:25:29 +0100

    test tools

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -8,5 +8,7 @@ from prediction_market_agent.tools.web_scrape import _summary, web_scrape
 def test_search_and_scrape() -> None:
     objective = "In 2024, will Israel conduct a military strike against Iran?"
     search = GoogleSearchTool().fn(query=objective)
-    scraping = web_scrape(objective, search[0])
-    print(_summary(objective, scraping))
+    assert (
+        "https://www.cnn.com/middleeast/live-news/israel-hamas-war-gaza-news-01-20-24/index.html"
+        in search
+    )
