31656341503fde30fef4c88d5ea26e911bc5edc1
Peter Jung <peter@jung.ninja>
2024-01-25 14:49:45 +0100

    fix test

--- tests/test_manifold.py
+++ tests/test_manifold.py
@@ -1,8 +1,11 @@
+import pytest
+from tests.utils import RUN_PAID_TESTS
 from prediction_market_agent import utils
 from prediction_market_agent.markets import manifold
 from prediction_market_agent.tools.gtypes import mana_type
 
 
+@pytest.mark.skipif(not RUN_PAID_TESTS, reason="This test costs money to run.")
 def test_manifold() -> None:
     market = manifold.pick_binary_market()
     print("Placing bet on market:", market.question)
