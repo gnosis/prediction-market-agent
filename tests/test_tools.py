0344cbb9f29097e95ac933a6cfe55d0015fb59cd
Peter Jung <peter@jung.ninja>
2024-01-25 10:30:46 +0100

    maybe fix

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,7 +1,10 @@
+import pytest
+from prediction_market_agent import utils
 from prediction_market_agent.tools.google_search import GoogleSearchTool
 from prediction_market_agent.tools.web_scrape import _summary, web_scrape
 
 
+@pytest.mark.skipif(utils.get_serp_api_key() is None, reason="No Serp API key")
 def test_search_and_scrape() -> None:
     objective = "In 2024, will Israel conduct a military strike against Iran?"
     search = GoogleSearchTool().fn(query=objective)
