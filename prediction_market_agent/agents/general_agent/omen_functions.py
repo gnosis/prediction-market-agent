6479bc85a88534e2c9c0628f7aeabfb4fb44b111
Gabriel Fior <fior.gbm@gmail.com>
2024-04-07 22:52:54 -0300

    Added getMarkets function and passed it to agent

---
+++ prediction_market_agent/agents/general_agent/omen_functions.py
@@ -0,0 +1,24 @@
+import sys
+
+from crewai_tools import BaseTool
+from prediction_market_agent_tooling.markets.agent_market import AgentMarket, FilterBy
+from prediction_market_agent_tooling.markets.markets import get_binary_markets, MarketType
+
+
+# ToDo - Add filters as arguments to this function call, add it to description.
+class GetBinaryMarketsTool(BaseTool):
+    name: str = "Tool for fetching binary markets"
+    description: str = "This tool returns all the markets on the prediction market platform Omen."
+
+    def _run(self) -> list[AgentMarket]:
+        # Implementation goes here
+        markets = get_binary_markets(
+            limit=10,
+            market_type=MarketType.OMEN,
+            filter_by=FilterBy.NONE,
+        )
+        return markets
+
+    def cache_function(self, args, result) -> bool:
+        # ToDo - implement caching if result was > 1 and depending on filter.
+        return False
