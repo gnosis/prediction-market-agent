2863906436eb9c51570365528088820cfa3a0c46
Evan Griffiths <56087052+evangriffiths@users.noreply.github.com>
2024-04-10 11:08:18 +0100

    Make microchain Functions (somewhat) market-type agnostic (#60)
    * Make microchain Functions (somewhat) market-type agnostic

--- prediction_market_agent/agents/microchain_agent/utils.py
+++ prediction_market_agent/agents/microchain_agent/utils.py
@@ -1,13 +1,14 @@
-import os
-from typing import List, cast
-
 from eth_typing import ChecksumAddress
 from prediction_market_agent_tooling.markets.agent_market import (
     AgentMarket,
     FilterBy,
     SortBy,
 )
-from prediction_market_agent_tooling.markets.omen.omen import OmenAgentMarket
+from prediction_market_agent_tooling.markets.markets import MarketType
+from prediction_market_agent_tooling.markets.omen.data_models import (
+    OMEN_FALSE_OUTCOME,
+    OMEN_TRUE_OUTCOME,
+)
 from prediction_market_agent_tooling.markets.omen.omen_contracts import (
     OmenConditionalTokenContract,
 )
@@ -15,8 +16,7 @@ from prediction_market_agent_tooling.markets.omen.omen_subgraph_handler import (
     OmenSubgraphHandler,
 )
 from prediction_market_agent_tooling.tools.hexbytes_custom import HexBytes
-from prediction_market_agent_tooling.tools.web3_utils import private_key_to_public_key
-from pydantic import BaseModel, SecretStr
+from pydantic import BaseModel
 from web3.types import Wei
 
 
@@ -25,7 +25,7 @@ class MicroMarket(BaseModel):
     p_yes: float
 
     @staticmethod
-    def from_agent_market(market: OmenAgentMarket) -> "MicroMarket":
+    def from_agent_market(market: AgentMarket) -> "MicroMarket":
         return MicroMarket(
             question=market.question,
             p_yes=float(market.p_yes),
@@ -35,25 +35,32 @@ class MicroMarket(BaseModel):
         return f"'{self.question}' with probability of yes: {self.p_yes:.2%}"
 
 
-def get_omen_binary_markets() -> list[OmenAgentMarket]:
+def get_binary_markets(market_type: MarketType) -> list[AgentMarket]:
     # Get the 5 markets that are closing soonest
-    markets: list[AgentMarket] = OmenAgentMarket.get_binary_markets(
+    cls = market_type.market_class
+    markets: list[AgentMarket] = cls.get_binary_markets(
         filter_by=FilterBy.OPEN,
-        sort_by=SortBy.CLOSING_SOONEST,
+        sort_by=(
+            SortBy.NONE
+            if market_type == MarketType.POLYMARKET
+            else SortBy.CLOSING_SOONEST
+        ),
         limit=5,
     )
-    return cast(List[OmenAgentMarket], markets)
+    return markets
 
 
-def get_omen_binary_market_from_question(market: str) -> OmenAgentMarket:
-    markets = get_omen_binary_markets()
+def get_binary_market_from_question(
+    market: str, market_type: MarketType
+) -> AgentMarket:
+    markets = get_binary_markets(market_type=market_type)
     for m in markets:
         if m.question == market:
             return m
     raise ValueError(f"Market '{market}' not found")
 
 
-def get_omen_market_token_balance(
+def get_market_token_balance(
     user_address: ChecksumAddress, market_condition_id: HexBytes, market_index_set: int
 ) -> Wei:
     # We get the multiple positions for each market
@@ -67,8 +74,15 @@ def get_omen_market_token_balance(
     return balance
 
 
-def fetch_public_key_from_env() -> ChecksumAddress:
-    private_key = os.environ.get("BET_FROM_PRIVATE_KEY")
-    if private_key is None:
-        raise EnvironmentError("Could not load private key using env var")
-    return private_key_to_public_key(SecretStr(private_key))
+def get_yes_outcome(market_type: MarketType) -> str:
+    if market_type == MarketType.OMEN:
+        return OMEN_TRUE_OUTCOME
+    else:
+        raise ValueError(f"Market type '{market_type}' not supported")
+
+
+def get_no_outcome(market_type: MarketType) -> str:
+    if market_type == MarketType.OMEN:
+        return OMEN_FALSE_OUTCOME
+    else:
+        raise ValueError(f"Market type '{market_type}' not supported")
--- tests/agents/test_microchain.py
+++ tests/agents/test_microchain.py
@@ -1,8 +1,6 @@
-import typing as t
-
 import pytest
-from dotenv import load_dotenv
 from eth_typing import HexAddress, HexStr
+from prediction_market_agent_tooling.markets.markets import MarketType
 from prediction_market_agent_tooling.markets.omen.omen import OmenAgentMarket
 from prediction_market_agent_tooling.markets.omen.omen_subgraph_handler import (
     OmenSubgraphHandler,
@@ -18,8 +16,8 @@ from prediction_market_agent.agents.microchain_agent.functions import (
     GetWalletBalance,
 )
 from prediction_market_agent.agents.microchain_agent.utils import (
-    get_omen_binary_markets,
-    get_omen_market_token_balance,
+    get_binary_markets,
+    get_market_token_balance,
 )
 from tests.utils import RUN_PAID_TESTS
 
@@ -27,45 +25,47 @@ REPLICATOR_ADDRESS = "0x993DFcE14768e4dE4c366654bE57C21D9ba54748"
 AGENT_0_ADDRESS = "0x2DD9f5678484C1F59F97eD334725858b938B4102"
 
 
-@pytest.fixture(scope="session", autouse=True)
-def before_all_tests() -> t.Generator[None, None, None]:
-    load_dotenv()
-    yield None
-
-
-def test_get_markets() -> None:
-    get_markets = GetMarkets()
+# TODO investigate why this fails for polymarket https://github.com/gnosis/prediction-market-agent/issues/62
+@pytest.mark.parametrize("market_type", [MarketType.OMEN, MarketType.MANIFOLD])
+def test_get_markets(market_type: MarketType) -> None:
+    get_markets = GetMarkets(market_type=market_type)
     assert len(get_markets()) > 0
 
 
 @pytest.mark.skipif(not RUN_PAID_TESTS, reason="This test costs money to run.")
-def test_buy_yes() -> None:
-    market = get_omen_binary_markets()[0]
-    buy_yes = BuyYes()
+@pytest.mark.parametrize("market_type", [MarketType.OMEN])
+def test_buy_yes(market_type: MarketType) -> None:
+    market = get_binary_markets(market_type=market_type)[0]
+    buy_yes = BuyYes(market_type=market_type)
     print(buy_yes(market.question, 0.0001))
 
 
 @pytest.mark.skipif(not RUN_PAID_TESTS, reason="This test costs money to run.")
-def test_buy_no() -> None:
-    market = get_omen_binary_markets()[0]
-    buy_yes = BuyNo()
+@pytest.mark.parametrize("market_type", [MarketType.OMEN])
+def test_buy_no(market_type: MarketType) -> None:
+    market = get_binary_markets(market_type=market_type)[0]
+    buy_yes = BuyNo(market_type=market_type)
     print(buy_yes(market.question, 0.0001))
 
 
-def test_replicator_has_balance_gt_0() -> None:
-    balance = GetWalletBalance()(REPLICATOR_ADDRESS)
+@pytest.mark.parametrize("market_type", [MarketType.OMEN])
+def test_replicator_has_balance_gt_0(market_type: MarketType) -> None:
+    balance = GetWalletBalance(market_type=market_type)(REPLICATOR_ADDRESS)
     assert balance > 0
 
 
-def test_agent_0_has_bet_on_market() -> None:
-    user_positions = GetUserPositions()(AGENT_0_ADDRESS)
+@pytest.mark.parametrize("market_type", [MarketType.OMEN])
+def test_agent_0_has_bet_on_market(market_type: MarketType) -> None:
+    user_positions = GetUserPositions(market_type=market_type)(AGENT_0_ADDRESS)
     # Assert 3 conditionIds are included
     expected_condition_ids = [
         HexBytes("0x9c7711bee0902cc8e6838179058726a7ba769cc97d4d0ea47b31370d2d7a117b"),
         HexBytes("0xe2bf80af2a936cdabeef4f511620a2eec46f1caf8e75eb5dc189372367a9154c"),
         HexBytes("0x3f8153364001b26b983dd92191a084de8230f199b5ad0b045e9e1df61089b30d"),
     ]
-    unique_condition_ids = sum([u.position.conditionIds for u in user_positions], [])
+    unique_condition_ids: list[HexBytes] = sum(
+        [u.position.conditionIds for u in user_positions], []
+    )
     assert set(expected_condition_ids).issubset(unique_condition_ids)
 
 
@@ -77,7 +77,7 @@ def test_balance_for_user_in_market() -> None:
     )  # yes/no
     market = subgraph_handler.get_omen_market(market_id)
     omen_agent_market = OmenAgentMarket.from_data_model(market)
-    balance_yes = get_omen_market_token_balance(
+    balance_yes = get_market_token_balance(
         user_address=Web3.to_checksum_address(user_address),
         market_condition_id=omen_agent_market.condition.id,
         market_index_set=market.condition.index_sets[0],
@@ -85,7 +85,7 @@ def test_balance_for_user_in_market() -> None:
 
     assert balance_yes == 1959903969410997
 
-    balance_no = get_omen_market_token_balance(
+    balance_no = get_market_token_balance(
         user_address=Web3.to_checksum_address(user_address),
         market_condition_id=omen_agent_market.condition.id,
         market_index_set=market.condition.index_sets[1],
