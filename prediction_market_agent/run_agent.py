45973f27b5f26003c9fb3f6efb78af7a8c5486b0
Peter Jung <peter@jung.ninja>
2024-04-11 14:36:28 +0200

    rename

--- prediction_market_agent/run_agent.py
+++ prediction_market_agent/run_agent.py
@@ -11,7 +11,7 @@ from enum import Enum
 import typer
 from prediction_market_agent_tooling.markets.markets import MarketType
 
-from prediction_market_agent.agents.coinflip_agent.coinflip_agent import (
+from prediction_market_agent.agents.coinflip_agent.deploy import (
     DeployableCoinFlipAgent,
 )
 
