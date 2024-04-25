1416bec8edfdb5a0ff326af2a328b7b02776ddcf
Peter Jung <peter@jung.ninja>
2024-04-25 10:35:15 +0200

    Move replication functions (#109)

---
+++ prediction_market_agent/agents/replicate_to_omen_agent/omen_resolve_replicated.py
@@ -0,0 +1,90 @@
+from datetime import timedelta
+
+from loguru import logger
+from prediction_market_agent_tooling.config import PrivateCredentials
+from prediction_market_agent_tooling.gtypes import HexAddress, HexBytes
+from prediction_market_agent_tooling.markets.omen.data_models import RealityQuestion
+from prediction_market_agent_tooling.markets.omen.omen_resolving import (
+    claim_bonds_on_realitio_questions,
+    finalize_markets,
+    find_resolution_on_other_markets,
+    resolve_markets,
+)
+from prediction_market_agent_tooling.markets.omen.omen_subgraph_handler import (
+    OmenSubgraphHandler,
+)
+from prediction_market_agent_tooling.tools.balances import get_balances
+from prediction_market_agent_tooling.tools.utils import utcnow
+from pydantic import BaseModel
+
+
+class FinalizeAndResolveResult(BaseModel):
+    finalized: list[HexAddress]
+    resolved: list[HexAddress]
+    claimed_question_ids: list[HexBytes]
+
+
+def omen_finalize_and_resolve_and_claim_back_all_markets_based_on_others_tx(
+    private_credentials: PrivateCredentials,
+) -> FinalizeAndResolveResult:
+    public_key = private_credentials.public_key
+    balances_start = get_balances(public_key)
+    logger.info(f"{balances_start=}")
+
+    # Just to be friendly with timezones.
+    before = utcnow() - timedelta(hours=8)
+
+    # Fetch markets created by us that are already open, but no answer was submitted yet.
+    created_opened_markets = OmenSubgraphHandler().get_omen_binary_markets(
+        limit=None,
+        creator=public_key,
+        opened_before=before,
+        finalized=False,
+    )
+    # Finalize them (set answer on Realitio).
+    created_opened_markets_with_resolutions = [
+        (m, find_resolution_on_other_markets(m)) for m in created_opened_markets
+    ]
+    finalized_markets = finalize_markets(
+        private_credentials,
+        created_opened_markets_with_resolutions,
+    )
+    balances_after_finalization = get_balances(public_key)
+    logger.info(f"{balances_after_finalization=}")
+
+    # Fetch markets created by us that are already open, and we already submitted an answer more than a day ago, but they aren't resolved yet.
+    created_finalized_markets = OmenSubgraphHandler().get_omen_binary_markets(
+        limit=None,
+        creator=public_key,
+        finalized_before=before - timedelta(hours=24),
+        resolved=False,
+    )
+    # Resolve them (resolve them on Oracle).
+    resolved_markets = resolve_markets(
+        private_credentials,
+        created_finalized_markets,
+    )
+    balances_after_resolution = get_balances(public_key)
+    logger.info(f"{balances_after_resolution=}")
+
+    # Fetch questions that are already finalised (last answer is older than 24 hours), but we didn't claim the bonded xDai yet.
+    created_not_claimed_questions: list[
+        RealityQuestion
+    ] = OmenSubgraphHandler().get_questions(
+        user=public_key,
+        claimed=False,
+        current_answer_before=before - timedelta(hours=24),
+    )
+    claimed_question_ids = claim_bonds_on_realitio_questions(
+        private_credentials,
+        created_not_claimed_questions,
+        auto_withdraw=True,
+    )
+    balances_after_claiming = get_balances(public_key)
+    logger.info(f"{balances_after_claiming=}")
+
+    return FinalizeAndResolveResult(
+        finalized=finalized_markets,
+        resolved=resolved_markets,
+        claimed_question_ids=claimed_question_ids,
+    )
---
+++ scripts/resolve_replicated_on_omen.py
@@ -0,0 +1,43 @@
+from pprint import pprint
+
+import typer
+from prediction_market_agent_tooling.config import PrivateCredentials
+from prediction_market_agent_tooling.gtypes import private_key_type
+from web3 import Web3
+
+from prediction_market_agent.agents.replicate_to_omen_agent.omen_resolve_replicated import (
+    omen_finalize_and_resolve_and_claim_back_all_markets_based_on_others_tx,
+)
+
+# Use without the pretty exceptions, because they make the error stack unusable here.
+app = typer.Typer(pretty_exceptions_show_locals=False)
+
+
+@app.command()
+def main(
+    from_private_key: str = typer.Option(),
+    safe_address: str = typer.Option(default=None),
+) -> None:
+    """
+    Helper script to resolve markets on Omen that were replicated by the replication function.
+
+    ```bash
+    python scripts/resolve_replicated_on_omen.py --from-private-key your-private-key
+    ```
+    """
+    safe_address_checksum = (
+        Web3.to_checksum_address(safe_address) if safe_address else None
+    )
+    private_credentials = PrivateCredentials(
+        private_key=private_key_type(from_private_key),
+        safe_address=safe_address_checksum,
+    )
+
+    result = omen_finalize_and_resolve_and_claim_back_all_markets_based_on_others_tx(
+        private_credentials=private_credentials
+    )
+    pprint(result.model_dump())
+
+
+if __name__ == "__main__":
+    app()
