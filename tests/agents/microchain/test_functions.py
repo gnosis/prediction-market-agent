37be5899dd66d58ead8a729b27bed6cc8172c52a
evangriffiths <egriffiths93@gmail.com>
2024-05-01 15:13:41 +0100

    Fix

--- tests/agents/microchain/test_functions.py
+++ tests/agents/microchain/test_functions.py
@@ -15,8 +15,6 @@ from prediction_market_agent.agents.microchain_agent.functions import (
     GetMarketProbability,
     GetMarkets,
     GetPositions,
-    MarketFunction,
-    PredictProbabilityForQuestionLocal,
     PredictProbabilityForQuestionRemote,
     SellNo,
     SellYes,
@@ -156,21 +154,12 @@ def test_buy_sell_tokens(market_type: MarketType) -> None:
 
 
 @pytest.mark.skipif(not RUN_PAID_TESTS, reason="This test costs money to run.")
-@pytest.mark.parametrize(
-    "prediction_method",
-    [
-        PredictProbabilityForQuestionRemote,
-        PredictProbabilityForQuestionLocal,
-    ],
-)
 @pytest.mark.parametrize("market_type", [MarketType.OMEN])
-def test_predict_probability(
-    market_type: MarketType, prediction_method: MarketFunction
-) -> None:
+def test_predict_probability(market_type: MarketType) -> None:
     """
     Test calling a mech to predict the probability of a market
     """
-    predict_probability = prediction_method(market_type=market_type)
+    predict_probability = PredictProbabilityForQuestionRemote(market_type=market_type)
     market = get_binary_markets(market_type=market_type)[0]
     p_yes = predict_probability(market.id)
     assert 0.0 <= float(p_yes) <= 1.0
