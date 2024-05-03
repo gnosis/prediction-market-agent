a785374822426bc37a8a99434df87e773c5abb41
Gabriel Fior <fior.gbm@gmail.com>
2024-05-03 20:13:08 -0300

    Added placeholder for farcaster_client.py

---
+++ prediction_market_agent/agents/autogen_general_agent/farcaster_client.py
@@ -0,0 +1,9 @@
+# ToDo - Add farcaster-client
+# ToDo - Add vars to env
+
+# To use the Warpcast API you need to have a Farcaster account. We will use the mnemonic or private key of the Farcaster custody account (not your main wallet) to connect to the API.
+# from farcaster import Warpcast
+# client = Warpcast(mnemonic=os.environ.get("<MNEMONIC_ENV_VAR>"))
+# print(client.get_healthcheck())
+# response = client.post_cast(text="Hello world!")
+# print(response.cast.hash) # "0x...."
