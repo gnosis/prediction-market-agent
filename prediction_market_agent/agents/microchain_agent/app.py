7f2959c6ed93f58274f20785e894313acafb7e0d
evangriffiths <egriffiths93@gmail.com>
2024-05-14 17:42:02 +0100

    Change default model

--- prediction_market_agent/agents/microchain_agent/app.py
+++ prediction_market_agent/agents/microchain_agent/app.py
@@ -101,7 +101,7 @@ with st.sidebar:
     if not agent_is_initialized():
         model = st.selectbox(
             "Model",
-            ["gpt-4-turbo-2024-04-09", "gpt-3.5-turbo-0125"],
+            ["gpt-3.5-turbo-0125", "gpt-4-turbo-2024-04-09"],
             index=0,
         )
         if model is None:
