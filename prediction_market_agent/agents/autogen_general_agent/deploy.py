bc17f3015ae02ac8f8ed3c6c028cefaaf18a81f6
Gabriel Fior <fior.gbm@gmail.com>
2024-05-14 10:31:11 -0300

    Fixed isort (2)

--- prediction_market_agent/agents/autogen_general_agent/deploy.py
+++ prediction_market_agent/agents/autogen_general_agent/deploy.py
@@ -1,4 +1,4 @@
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta
 
 from prediction_market_agent_tooling.deploy.agent import DeployableAgent
 from prediction_market_agent_tooling.loggers import logger
