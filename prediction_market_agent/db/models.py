b85080200518d0d7de6552848190839c07679d73
evangriffiths <egriffiths93@gmail.com>
2024-05-14 17:38:33 +0100

    remove hack

--- prediction_market_agent/db/models.py
+++ prediction_market_agent/db/models.py
@@ -6,7 +6,6 @@ from sqlmodel import Field, SQLModel
 
 class LongTermMemories(SQLModel, table=True):
     __tablename__ = "long_term_memories"
-    __table_args__ = {"extend_existing": True}
     id: Optional[int] = Field(default=None, primary_key=True)
     task_description: str
     metadata_: Optional[str] = None
