ceb0662ea3d4cd81a2306c64cfba7ab1d346dc96
Gabriel Fior <fior.gbm@gmail.com>
2024-05-20 17:39:37 +0200

    Small fix deploy.py

--- prediction_market_agent/agents/think_thoroughly_agent/deploy.py
+++ prediction_market_agent/agents/think_thoroughly_agent/deploy.py
@@ -5,7 +5,6 @@ from prediction_market_agent_tooling.markets.markets import MarketType
 from prediction_market_agent.agents.think_thoroughly_agent.think_thoroughly_agent import (
     CrewAIAgentSubquestions,
 )
-from prediction_market_agent.utils import APIKeys
 
 
 class DeployableThinkThoroughlyAgent(DeployableTraderAgent):
@@ -21,14 +20,6 @@ class DeployableThinkThoroughlyAgent(DeployableTraderAgent):
 
 if __name__ == "__main__":
     agent = DeployableThinkThoroughlyAgent(place_bet=False)
-    # from crewai.utilities import paths
-    # db_path = f"{paths.db_storage_path()}/long_term_memory_storage.db"
-    # print(db_path)
-    # ToDo delete me
-    import os
-
-    k = APIKeys()
-    os.environ["OPENAI_API_KEY"] = k.openai_api_key.get_secret_value()
     agent.deploy_local(
         market_type=MarketType.OMEN,
         sleep_time=540,
