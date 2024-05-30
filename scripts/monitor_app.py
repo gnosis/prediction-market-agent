37ed0e2e16ca150e74d299104d28dd496460789c
Peter Jung <peter@jung.ninja>
2024-05-30 10:45:11 +0200

    Add monitor script (#219)

---
+++ scripts/monitor_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from prediction_market_agent_tooling.monitor.monitor_app import MarketType, monitor_app
+
+if __name__ == "__main__":
+    st.set_page_config(layout="wide")  # Best viewed with a wide screen
+    st.title("Monitoring")
+    monitor_app(enabled_market_types=[MarketType.MANIFOLD, MarketType.OMEN])
