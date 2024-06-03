723816e14cbcb34cbcac85b189d0d7864fc34627
Gabriel Fior <fior.gbm@gmail.com>
2024-06-03 18:45:38 -0300

    Fixed isort

--- prediction_market_agent/agents/autogen_general_agent/social_agent.py
+++ prediction_market_agent/agents/autogen_general_agent/social_agent.py
@@ -18,9 +18,7 @@ from prediction_market_agent.agents.microchain_agent.memory import (
     LongTermMemory,
     SimpleMemoryThinkThoroughly,
 )
-from prediction_market_agent.agents.utils import (
-    extract_reasonings_to_learnings,
-)
+from prediction_market_agent.agents.utils import extract_reasonings_to_learnings
 from prediction_market_agent.utils import APIKeys
 
 
