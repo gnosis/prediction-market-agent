b36a4c74297ae067071d4857d25157455a30a76a
evangriffiths <egriffiths93@gmail.com>
2024-06-21 11:28:23 +0100

    Remove ChatHistory.add_message, add test

--- tests/test_chat_history.py
+++ tests/test_chat_history.py
@@ -1,12 +1,27 @@
 from datetime import datetime
+from typing import Generator
+
+import pytest
 
 from prediction_market_agent.agents.microchain_agent.memory import (
     DatedChatHistory,
     DatedChatMessage,
+    LongTermMemory,
 )
 
 
-def test_chat_history_clustering() -> None:
+@pytest.fixture(scope="session")
+def long_term_memory() -> Generator[LongTermMemory, None, None]:
+    """Creates a in-memory SQLite DB for testing"""
+    long_term_memory = LongTermMemory(
+        task_description="test", sqlalchemy_db_url="sqlite://"
+    )
+    long_term_memory.storage._initialize_db()
+    yield long_term_memory
+
+
+@pytest.fixture(scope="session")
+def chat_history() -> DatedChatHistory:
     chat_messages = [
         DatedChatMessage(
             content="foo", role="user", datetime_=datetime(2022, 1, 1, 0, 0)
@@ -18,7 +33,10 @@ def test_chat_history_clustering() -> None:
             content="baz", role="user", datetime_=datetime(2022, 1, 1, 0, 25)
         ),
     ]
-    chat_history = DatedChatHistory(chat_messages=chat_messages)
+    return DatedChatHistory(chat_messages=chat_messages)
+
+
+def test_chat_history_clustering(chat_history: DatedChatHistory) -> None:
     assert chat_history.num_messages == 3
 
     clusters0 = chat_history.cluster_by_datetime(max_minutes_between_messages=10)
@@ -32,3 +50,12 @@ def test_chat_history_clustering() -> None:
 
     # Check that chat_history is still the same length after clustering
     assert chat_history.num_messages == 3
+
+
+def test_save_to_memory(
+    long_term_memory: LongTermMemory, chat_history: DatedChatHistory
+) -> None:
+    datetime_now = datetime.now()
+    chat_history.save_to(long_term_memory)
+    breakpoint()
+    assert long_term_memory.search(from_=datetime_now)
