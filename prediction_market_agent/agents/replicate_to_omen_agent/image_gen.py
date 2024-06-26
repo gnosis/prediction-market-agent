fd7141fa39ed209530f4860a3d0770a9e168c562
Peter Jung <peter@jung.ninja>
2024-06-26 15:26:28 +0200

    Fix image generation in replicator

--- prediction_market_agent/agents/replicate_to_omen_agent/image_gen.py
+++ prediction_market_agent/agents/replicate_to_omen_agent/image_gen.py
@@ -2,10 +2,10 @@ import os
 from tempfile import TemporaryDirectory
 
 from pinatapy import PinataPy
-from prediction_market_agent_tooling.gtypes import IPFSCIDVersion0
+from prediction_market_agent_tooling.gtypes import ChecksumAddress, IPFSCIDVersion0
 from prediction_market_agent_tooling.loggers import logger
-from prediction_market_agent_tooling.markets.omen.omen import OmenAgentMarket
 from prediction_market_agent_tooling.markets.omen.omen_contracts import (
+    OmenFixedProductMarketMakerContract,
     OmenThumbnailMapping,
 )
 from prediction_market_agent_tooling.tools.image_gen.market_thumbnail_gen import (
@@ -16,8 +16,13 @@ from prediction_market_agent.utils import APIKeys
 
 
 def generate_and_set_image_for_market(
-    market: OmenAgentMarket, api_keys: APIKeys
+    market_address: ChecksumAddress, market_question: str, api_keys: APIKeys
 ) -> IPFSCIDVersion0 | None:
+    market_contract = OmenFixedProductMarketMakerContract(address=market_address)
+    # Test that the market actually exists.
+    # TODO: How can we get the question from the market contract (without using the graph)? (to check that `market_question` is correct)
+    market_contract.totalSupply()
+
     image_mapping_contract = OmenThumbnailMapping()
     pinata = PinataPy(
         api_keys.pinata_api_key.get_secret_value(),
@@ -25,12 +30,12 @@ def generate_and_set_image_for_market(
     )
 
     try:
-        generated_image = generate_image_for_market(question=market.question)
+        generated_image = generate_image_for_market(question=market_question)
     except Exception as e:
         # Roughly one every 30 markets triggers OpenAI's content policy violation, because of the prompt content. We can just skip those.
         if "content_policy_violation" in str(e):
             logger.warning(
-                f"Content policy violation for {market.url}: {market.question}"
+                f"Content policy violation for {market_address=}: {market_question}"
             )
             return None
         raise
@@ -42,8 +47,6 @@ def generate_and_set_image_for_market(
             pinata.pin_file_to_ipfs(image_path, save_absolute_paths=False)["IpfsHash"]
         )
 
-    image_mapping_contract.set(
-        api_keys, market.market_maker_contract_address_checksummed, ipfs_hash
-    )
+    image_mapping_contract.set(api_keys, market_address, ipfs_hash)
 
     return ipfs_hash
