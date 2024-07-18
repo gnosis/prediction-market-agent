c72c2019b33aac6bf631d57c6e50fdca8f57846f
Peter Jung <peter@jung.ninja>
2024-07-18 09:38:38 +0200

    More granual replication logic

--- prediction_market_agent/agents/replicate_to_omen_agent/deploy.py
+++ prediction_market_agent/agents/replicate_to_omen_agent/deploy.py
@@ -8,6 +8,7 @@ from prediction_market_agent_tooling.markets.omen.omen import (
     redeem_from_all_user_positions,
 )
 from prediction_market_agent_tooling.tools.utils import utcnow
+from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from web3 import Web3
 
@@ -25,14 +26,22 @@ REPLICATOR_ADDRESS = Web3.to_checksum_address(
 )
 
 
+class ReplicateConfig(BaseModel):
+    n: int  # How many markets of this configuration to replicate.
+    close_time_up_to_n_days: (
+        int  # Consider only markets closing in less than N days for this configuration.
+    )
+    every_n_days: int  # This configuration should execute every N days.
+    sources: list[MarketType] = [MarketType.MANIFOLD, MarketType.POLYMARKET]
+
+
 class ReplicateSettings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
 
-    N_TO_REPLICATE: int
+    REPLICATE: list[ReplicateConfig]
     INITIAL_FUNDS: str
-    CLOSE_TIME_UP_TO_N_DAYS: list[int]
 
 
 class DeployableReplicateToOmenAgent(DeployableAgent):
@@ -42,6 +51,7 @@ class DeployableReplicateToOmenAgent(DeployableAgent):
 
         keys = APIKeys()
         settings = ReplicateSettings()
+        now = utcnow()
 
         logger.info(
             f"Finalising, resolving and claiming back xDai from existing markets replicated by {keys.bet_from_address}."
@@ -56,22 +66,34 @@ class DeployableReplicateToOmenAgent(DeployableAgent):
         logger.info("Redeeming funds from previously unfunded markets.")
         redeem_from_all_user_positions(keys)
 
-        for replicate_from_market_type in [MarketType.MANIFOLD, MarketType.POLYMARKET]:
-            for close_time_days in settings.CLOSE_TIME_UP_TO_N_DAYS:
-                close_time_before = utcnow() + timedelta(days=close_time_days)
+        for replicate_config in settings.REPLICATE:
+            if now.timetuple().tm_yday % replicate_config.every_n_days:
+                logger.info(f"Skipping {replicate_config}, because it's not his day.")
+                continue
+
+            to_replicate = replicate_config.n
+
+            for replicate_from_market_type in replicate_config.sources:
+                close_time_before = now + timedelta(
+                    days=replicate_config.close_time_up_to_n_days
+                )
                 initial_funds_per_market = xdai_type(settings.INITIAL_FUNDS)
 
                 logger.info(
-                    f"Replicating from {replicate_from_market_type} markets closing in {close_time_days} days."
+                    f"Replicating {to_replicate} from {replicate_from_market_type} markets closing in {replicate_config.close_time_up_to_n_days} days."
                 )
-                omen_replicate_from_tx(
+                replicated = omen_replicate_from_tx(
                     market_type=replicate_from_market_type,
-                    n_to_replicate=settings.N_TO_REPLICATE,
+                    n_to_replicate=to_replicate,
                     initial_funds=initial_funds_per_market,
                     api_keys=keys,
                     close_time_before=close_time_before,
                     auto_deposit=True,
                 )
+                to_replicate -= len(replicated)
+
+                if to_replicate <= 0:
+                    break
 
             logger.info(f"Replication from {replicate_from_market_type} done.")
 
--- scripts/replicator_stats.py
+++ scripts/replicator_stats.py
@@ -6,6 +6,8 @@ from prediction_market_agent_tooling.markets.omen.omen import OmenAgentMarket
 from prediction_market_agent_tooling.markets.omen.omen_subgraph_handler import (
     OmenSubgraphHandler,
 )
+from prediction_market_agent_tooling.tools.parallelism import par_generator
+from tqdm import tqdm
 
 from prediction_market_agent.agents.replicate_to_omen_agent.deploy import (
     REPLICATOR_ADDRESS,
@@ -13,10 +15,34 @@ from prediction_market_agent.agents.replicate_to_omen_agent.deploy import (
 
 
 def main() -> None:
+    now = datetime.now()
     markets = OmenSubgraphHandler().get_omen_binary_markets(
         limit=None,
         creator=REPLICATOR_ADDRESS,
     )
+    bets_for_market = {
+        market.id: bets
+        for market, bets in tqdm(
+            par_generator(
+                markets,
+                lambda m: (
+                    m,
+                    OmenSubgraphHandler().get_bets(
+                        market_id=m.market_maker_contract_address_checksummed
+                    ),
+                ),
+            ),
+            total=len(markets),
+            desc="Loading bets",
+        )
+    }
+
+    open_markets_closing_in_less_than_30_days = [
+        m for m in markets if now < m.opening_datetime < now + timedelta(days=30)
+    ]
+    markets_closing_in_more_than_30_days = [
+        m for m in markets if m.opening_datetime > now + timedelta(days=30)
+    ]
 
     stats = {
         "markets created": len(markets),
@@ -38,8 +64,7 @@ def main() -> None:
         "liquidity in open markets closing in more than 30 days": sum(
             [
                 OmenAgentMarket.from_data_model(m).get_liquidity().amount
-                for m in markets
-                if m.opening_datetime > datetime.now() + timedelta(days=30)
+                for m in markets_closing_in_more_than_30_days
             ]
         ),
         "liquidity in open markets closing in more than 180 days": sum(
@@ -49,6 +74,15 @@ def main() -> None:
                 if m.opening_datetime > datetime.now() + timedelta(days=180)
             ]
         ),
+        "avg bets per market closing in less than 30 days": sum(
+            len(bets_for_market[m.id])
+            for m in open_markets_closing_in_less_than_30_days
+        )
+        / len(open_markets_closing_in_less_than_30_days),
+        "avg bets per market closing in more than 30 days": sum(
+            len(bets_for_market[m.id]) for m in markets_closing_in_more_than_30_days
+        )
+        / len(markets_closing_in_more_than_30_days),
     }
     pprint(stats)
 
