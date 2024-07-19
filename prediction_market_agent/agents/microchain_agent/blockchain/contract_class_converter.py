3dda0fafcf9f5922ceaf637c3f255c2065ea8df9
gabrielfior <fior.gbm@gmail.com>
2024-07-18 21:16:33 -0300

    Added description creation

--- prediction_market_agent/agents/microchain_agent/blockchain/contract_class_converter.py
+++ prediction_market_agent/agents/microchain_agent/blockchain/contract_class_converter.py
@@ -7,6 +7,9 @@ from microchain import Function
 from prediction_market_agent_tooling.tools.contract import ContractOnGnosisChain
 from web3 import Web3
 
+from prediction_market_agent.agents.microchain_agent.blockchain.code_interpreter import (
+    CodeInterpreter,
+)
 from prediction_market_agent.agents.microchain_agent.blockchain.models import (
     AbiItemTypeEnum,
     ABIMetadata,
@@ -44,12 +47,17 @@ class ContractClassConverter:
         self.session = requests_cache.CachedSession(backend="sqlite")
         self.contract_address = contract_address
 
-    def get_abi(self) -> list[ABIMetadata]:
+    def fetch_from_blockscout(self) -> dict[str, Any]:
         r = self.session.get(
             f"https://gnosis.blockscout.com/api/v2/smart-contracts/{self.contract_address}"
         )
         r.raise_for_status()
-        contract_abi = r.json()["abi"]
+        data: dict[str, Any] = r.json()
+        return data
+
+    def get_abi(self) -> list[ABIMetadata]:
+        data = self.fetch_from_blockscout()
+        contract_abi = data["abi"]
         # We extract only functions, not events
         return [
             ABIMetadata.parse_obj(abi_item)
@@ -57,10 +65,16 @@ class ContractClassConverter:
             if abi_item["type"] == "function"
         ]
 
+    def get_source_code(self) -> str:
+        data = self.fetch_from_blockscout()
+        source_code = data["source_code"]
+        return str(source_code)
+
     def generate_microchain_class_from_abi_item(
         self,
         abi_item: ABIMetadata,
         contract: ContractOnGnosisChain,
+        code_interpreter: CodeInterpreter,
     ):
         if abi_item.type != AbiItemTypeEnum.function:
             return None
@@ -90,36 +104,38 @@ class ContractClassConverter:
 
         function_code = f"def {abi_item.name}(self, {input_args}) -> {output_args}: return contract.call('{abi_item.name}', [{input_as_list}])"
 
-        print(function_code)
         namespace = {"contract": contract}
         exec(function_code, namespace)
         dynamic_function = namespace[abi_item.name]
 
-        # Example usage
-        factory = ClassFactory()
-
+        # Microchain specific attributes
         example_args = [TYPE_MAPPING[i.type][1] for i in abi_item.inputs]
+        # ToDo - Call generate_summary once instead of per function.
+        summary = code_interpreter.generate_summary(function_name=abi_item.name)
+
         attributes = {
             "__call__": dynamic_function,
-            "description": abi_item.name,
+            "description": summary,
             "example_args": example_args,
         }
 
-        DynamicClass = factory.create_class(
+        dynamic_class = ClassFactory().create_class(
             abi_item.name.title(), (Function,), attributes
         )
-        return DynamicClass
+        return dynamic_class
 
     def create_classes_from_smart_contract(self):
         # Get ABI from contract
         abi_items = self.get_abi()
+        source_code = self.get_source_code()
         # For each method, generate 1 class
         classes = []
         abi_str = json.dumps([i.model_dump() for i in abi_items])
         contract = ContractOnGnosisChain(abi=abi_str, address=self.contract_address)
+        code_interpreter = CodeInterpreter(source_code=source_code)
         for abi_item in abi_items:
             generated_class = self.generate_microchain_class_from_abi_item(
-                abi_item, contract
+                abi_item, contract, code_interpreter
             )
             if generated_class:
                 classes.append(generated_class)
