7df75c35150f5b4c95165bb445f039e587e66480
Peter Jung <peter@jung.ninja>
2024-08-27 13:57:20 +0200

    bugfix

--- prediction_market_agent/agents/omen_cleaner_agent/deploy.py
+++ prediction_market_agent/agents/omen_cleaner_agent/deploy.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 from prediction_market_agent_tooling.deploy.agent import DeployableAgent
-from prediction_market_agent_tooling.gtypes import HexAddress
+from prediction_market_agent_tooling.gtypes import HexAddress, HexStr
 from prediction_market_agent_tooling.loggers import logger
 from prediction_market_agent_tooling.markets.markets import MarketType
 from prediction_market_agent_tooling.markets.omen.omen_resolving import resolve_markets
@@ -14,6 +14,12 @@ from prediction_market_agent_tooling.tools.utils import utcnow
 from prediction_market_agent.utils import APIKeys
 
 CLEANER_TAG = "cleaner"
+
+# List of markets we know aren't resolvable and we fine to ignore them.
+IGNORED_FOR_RESOLUTION: list[HexAddress] = [
+    # Throws `Reverted condition not prepared or found`.
+    HexAddress(HexStr("0xf9b90903e98e68a1e69083490ee14c05444a57b4")),
+]
 
 
 class OmenCleanerAgent(DeployableAgent):
@@ -33,7 +39,13 @@ class OmenCleanerAgent(DeployableAgent):
     def resolve_finalized_markets(self, api_keys: APIKeys) -> list[HexAddress]:
         logger.info("Resolving finalized markets.")
         # Delay by 24 hours to give a chance for the market creator to be resolve it.
-        finalized_unresolved_markets = OmenSubgraphHandler().get_omen_binary_markets(
-            limit=None, finalized_before=utcnow() - timedelta(hours=24), resolved=False
-        )
+        finalized_unresolved_markets = [
+            market
+            for market in OmenSubgraphHandler().get_omen_binary_markets(
+                limit=None,
+                finalized_before=utcnow() - timedelta(hours=24),
+                resolved=False,
+            )
+            if market.id not in IGNORED_FOR_RESOLUTION
+        ]
         return resolve_markets(api_keys, finalized_unresolved_markets)
