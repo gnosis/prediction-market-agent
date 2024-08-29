97ec5ac60910915913aff1b89725da91f9b48f5b
evangriffiths <egriffiths93@gmail.com>
2024-08-29 15:06:09 +0100

    Review comment

--- prediction_market_agent/agents/utils.py
+++ prediction_market_agent/agents/utils.py
@@ -29,7 +29,7 @@ class AgentIdentifier(str, Enum):
     MICROCHAIN_AGENT_OMEN_LEARNING_2 = "general-agent-2"
     MICROCHAIN_AGENT_OMEN_LEARNING_3 = "general-agent-3"
     MICROCHAIN_AGENT_STREAMLIT = "microchain-streamlit-app"
-    MICROCHAIN_AGENT_OMEN_WITH_GOAL_MANAGER = "general-agent-4-with-goal-manager"
+    MICROCHAIN_AGENT_OMEN_WITH_GOAL_MANAGER = "trader-agent-0-with-goal-manager"
 
 
 MEMORIES_TO_LEARNINGS_TEMPLATE = """
