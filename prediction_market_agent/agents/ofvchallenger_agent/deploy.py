4f7bc1991f77f2af1e1bacc7b6e0554655b06aae
Peter Jung <peter@jung.ninja>
2024-09-05 13:32:25 +0200

    OFVChallenger (#440)

--- prediction_market_agent/agents/ofvchallenger_agent/deploy.py
+++ prediction_market_agent/agents/ofvchallenger_agent/deploy.py
@@ -1,10 +1,54 @@
-from prediction_market_agent_tooling.deploy.agent import DeployableAgent
-from prediction_market_agent_tooling.markets.markets import MarketType
-from prediction_market_agent_tooling.tools.langfuse_ import observe
+from datetime import timedelta
 
+from prediction_market_agent_tooling.deploy.agent import DeployableAgent
+from prediction_market_agent_tooling.gtypes import ChecksumAddress, xdai_type
+from prediction_market_agent_tooling.loggers import logger
+from prediction_market_agent_tooling.markets.markets import MarketType
+from prediction_market_agent_tooling.markets.omen.data_models import (
+    OmenMarket,
+    RealityResponse,
+)
+from prediction_market_agent_tooling.markets.omen.omen_resolving import (
+    Resolution,
+    omen_submit_answer_market_tx,
+    omen_submit_invalid_answer_market_tx,
+)
+from prediction_market_agent_tooling.markets.omen.omen_subgraph_handler import (
+    OmenSubgraphHandler,
+)
+from prediction_market_agent_tooling.tools.langfuse_ import observe
+from prediction_market_agent_tooling.tools.utils import utcnow
+from pydantic import BaseModel
+from web3 import Web3
+
+from prediction_market_agent.agents.ofvchallenger_agent.ofv_resolver import (
+    ofv_answer_binary_question,
+)
+from prediction_market_agent.agents.replicate_to_omen_agent.omen_resolve_replicated import (
+    claim_all_bonds_on_reality,
+)
 from prediction_market_agent.utils import APIKeys
 
 OFV_CHALLENGER_TAG = "ofv_challenger"
+OFV_CHALLENGER_EOA_ADDRESS = Web3.to_checksum_address(
+    "0x03aEEE267A985ab9835c025377F0D8209901a928"
+)
+OFV_CHALLENGER_SAFE_ADDRESS = Web3.to_checksum_address(
+    "0x9D0260500ba7b068b5b0f4AfA9F8864eBc0B059a"
+)
+CHALLENGE_BOND = xdai_type(10)
+# We benchmarked OFV only against Olas market-creators.
+MARKET_CREATORS_TO_CHALLENGE: list[ChecksumAddress] = [
+    # Olas market-creator 0.
+    Web3.to_checksum_address("0x89c5cc945dd550bcffb72fe42bff002429f46fec"),
+    # Olas market-creator 1.
+    Web3.to_checksum_address("0xffc8029154ecd55abed15bd428ba596e7d23f557"),
+]
+
+
+class Challenge(BaseModel):
+    old_responses: list[RealityResponse]
+    new_resolution: Resolution
 
 
 class OFVChallengerAgent(DeployableAgent):
@@ -12,10 +56,101 @@ class OFVChallengerAgent(DeployableAgent):
         if market_type != MarketType.OMEN:
             raise RuntimeError("Can challenge only Omen.")
 
-        self.challenge()
+        api_keys = APIKeys()
+        self.challenge(api_keys)
 
     @observe()
-    def challenge(self) -> None:
+    def challenge(self, api_keys: APIKeys) -> None:
         self.langfuse_update_current_trace(tags=[OFV_CHALLENGER_TAG])
-        APIKeys()
-        # TODO: Coming in the next PR.
+
+        # Claim the bonds as first thing, to have funds for the new challenges.
+        claim_all_bonds_on_reality(api_keys)
+
+        markets_open_for_answers = OmenSubgraphHandler().get_omen_binary_markets(
+            limit=None,
+            creator_in=MARKET_CREATORS_TO_CHALLENGE,
+            # We need markets already opened for answers.
+            opened_before=utcnow(),
+            # With a little bandwidth for the market to be finalized,
+            # so we have time for processing it without erroring out at the end.
+            finalized_after=utcnow() + timedelta(minutes=30),
+        )
+        logger.info(f"Found {len(markets_open_for_answers)} markets to challenge.")
+
+        for market in markets_open_for_answers:
+            self.challenge_market(market, api_keys)
+
+    @observe()
+    def challenge_market(
+        self,
+        market: OmenMarket,
+        api_keys: APIKeys,
+        web3: Web3 | None = None,
+    ) -> Challenge | None:
+        logger.info(f"Challenging market {market.url=}")
+
+        existing_responses = OmenSubgraphHandler().get_responses(
+            question_id=market.question.id
+        )
+        logger.info(
+            f"{market.url=}'s responses and bonds: {[(r.answer, r.bond_xdai) for r in existing_responses]}"
+        )
+
+        # We don't plan to re-challenge markets already challenged by the challenger, should we?
+        if any(
+            response.user_checksummed == OFV_CHALLENGER_SAFE_ADDRESS
+            for response in existing_responses
+        ):
+            logger.info(
+                f"Market {market.url=} already challenged by challenger. Skipping."
+            )
+            return None
+
+        # Next bond needs to be at least double the previous one.
+        if any(
+            response.bond_xdai >= CHALLENGE_BOND / 2 for response in existing_responses
+        ):
+            logger.info(
+                f"Market {market.url=} already challenged with bond > {CHALLENGE_BOND} / 2. Skipping."
+            )
+            return None
+
+        try:
+            answer = ofv_answer_binary_question(market.question_title, api_keys)
+        except Exception as e:
+            logger.exception(
+                f"Exception while getting factuality for market {market.url=}. Skipping. Exception: {e}"
+            )
+            return None
+
+        if answer is None:
+            logger.error(
+                f"Failed to get factuality for market {market.url=}, question {market.question_title=}. Skipping."
+            )
+            return None
+
+        new_resolution = (
+            Resolution.from_bool(answer.factuality)
+            if answer.factuality is not None
+            else Resolution.CANCEL
+        )
+        logger.info(
+            f"Challenging market {market.url=} with resolution {new_resolution=}"
+        )
+
+        if new_resolution != Resolution.CANCEL:
+            omen_submit_answer_market_tx(
+                api_keys=api_keys,
+                market=market,
+                resolution=new_resolution,
+                bond=CHALLENGE_BOND,
+                web3=web3,
+            )
+        else:
+            omen_submit_invalid_answer_market_tx(
+                api_keys=api_keys, market=market, bond=CHALLENGE_BOND, web3=web3
+            )
+
+        return Challenge(
+            old_responses=existing_responses, new_resolution=new_resolution
+        )
--- prediction_market_agent/agents/replicate_to_omen_agent/omen_resolve_replicated.py
+++ prediction_market_agent/agents/replicate_to_omen_agent/omen_resolve_replicated.py
@@ -1,9 +1,12 @@
-from datetime import timedelta
+from datetime import datetime, timedelta
 
 from prediction_market_agent_tooling.config import APIKeys
-from prediction_market_agent_tooling.gtypes import HexAddress, HexBytes
+from prediction_market_agent_tooling.gtypes import HexAddress, HexBytes, xDai, xdai_type
 from prediction_market_agent_tooling.loggers import logger
-from prediction_market_agent_tooling.markets.omen.data_models import RealityQuestion
+from prediction_market_agent_tooling.markets.omen.data_models import (
+    RealityQuestion,
+    RealityResponse,
+)
 from prediction_market_agent_tooling.markets.omen.omen_resolving import (
     claim_bonds_on_realitio_questions,
     finalize_markets,
@@ -14,16 +17,23 @@ from prediction_market_agent_tooling.markets.omen.omen_subgraph_handler import (
     OmenSubgraphHandler,
 )
 from prediction_market_agent_tooling.tools.balances import get_balances
+from prediction_market_agent_tooling.tools.langfuse_ import observe
 from prediction_market_agent_tooling.tools.utils import utcnow
 from pydantic import BaseModel
+
+
+class ClaimResult(BaseModel):
+    claimed_question_ids: list[HexBytes]
+    claimed: xDai
 
 
 class FinalizeAndResolveResult(BaseModel):
     finalized: list[HexAddress]
     resolved: list[HexAddress]
-    claimed_question_ids: list[HexBytes]
+    claimed: ClaimResult
 
 
+@observe()
 def omen_finalize_and_resolve_and_claim_back_all_markets_based_on_others_tx(
     api_keys: APIKeys,
 ) -> FinalizeAndResolveResult:
@@ -67,24 +77,45 @@ def omen_finalize_and_resolve_and_claim_back_all_markets_based_on_others_tx(
     balances_after_resolution = get_balances(public_key)
     logger.info(f"{balances_after_resolution=}")
 
-    # Fetch questions that are already finalised (last answer is older than 24 hours), but we didn't claim the bonded xDai yet.
-    created_not_claimed_questions: list[
-        RealityQuestion
-    ] = OmenSubgraphHandler().get_questions(
+    claimed = claim_all_bonds_on_reality(api_keys, finalized_before=before)
+
+    return FinalizeAndResolveResult(
+        finalized=finalized_markets,
+        resolved=resolved_markets,
+        claimed=claimed,
+    )
+
+
+@observe()
+def claim_all_bonds_on_reality(
+    api_keys: APIKeys, finalized_before: datetime | None = None
+) -> ClaimResult:
+    # Just to be friendly with time differences.
+    finalized_before = finalized_before or utcnow() - timedelta(hours=1)
+    public_key = api_keys.bet_from_address
+
+    balances_before_claiming = get_balances(public_key)
+    logger.info(f"{balances_before_claiming=}")
+
+    # Fetch our responses that are on already finalised questions, but we didn't claim the bonded xDai yet.
+    responses: list[RealityResponse] = OmenSubgraphHandler().get_responses(
         user=public_key,
-        claimed=False,
-        current_answer_before=before - timedelta(hours=24),
+        question_claimed=False,
+        question_finalized_before=finalized_before,
+    )
+    # Extract only the unique questions out of responses (there could be multiple responses for the same question, but only the whole question can be claimed once).
+    questions: list[RealityQuestion] = list(
+        {r.question.questionId: r.question for r in responses}.values()
     )
     claimed_question_ids = claim_bonds_on_realitio_questions(
         api_keys,
-        created_not_claimed_questions,
+        questions,
         auto_withdraw=True,
     )
     balances_after_claiming = get_balances(public_key)
     logger.info(f"{balances_after_claiming=}")
 
-    return FinalizeAndResolveResult(
-        finalized=finalized_markets,
-        resolved=resolved_markets,
+    return ClaimResult(
         claimed_question_ids=claimed_question_ids,
+        claimed=xdai_type(balances_after_claiming.xdai - balances_before_claiming.xdai),
     )
