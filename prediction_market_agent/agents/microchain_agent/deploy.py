920c0d2871f6f9e6dedf291ddce2969ad8e90269
Peter Jung <peter@jung.ninja>
2024-09-12 14:33:41 +0200

    Less iterations for Llama agent

--- prediction_market_agent/agents/microchain_agent/deploy.py
+++ prediction_market_agent/agents/microchain_agent/deploy.py
@@ -163,6 +163,8 @@ class DeployableMicrochainModifiableSystemPromptAgent3(
 ):
     task_description = AgentIdentifier.MICROCHAIN_AGENT_OMEN_LEARNING_3
     model = SupportedModel.llama_31_instruct
+    # Force less iterations, because Replicate's API allows at max 4096 input tokens.
+    n_iterations = 20
     description = "Microchain agent with 'just born' system prompt, and ability to adjust its own system prompt, version 3. Uses Llama 3.1 model."
 
 
