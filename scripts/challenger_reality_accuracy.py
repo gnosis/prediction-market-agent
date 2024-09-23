becb7b022a7e5a5f19b41eff60c4fdef9a40dc4f
Peter Jung <peter@jung.ninja>
2024-09-23 16:45:55 +0200

    Compute OFVChallenger accuracy

---
+++ scripts/challenger_reality_accuracy.py
@@ -0,0 +1,31 @@
+from datetime import timedelta
+
+from prediction_market_agent_tooling.tools.omen.reality_accuracy import reality_accuracy
+
+from prediction_market_agent.agents.ofvchallenger_agent.deploy import (
+    MARKET_CREATORS_TO_CHALLENGE,
+    OFV_CHALLENGER_SAFE_ADDRESS,
+)
+
+
+def main() -> None:
+    since = timedelta(days=4)
+    ofv_challenger_accuracy = reality_accuracy(OFV_CHALLENGER_SAFE_ADDRESS, since)
+    olas_accuracies = {
+        addr: reality_accuracy(addr, since) for addr in MARKET_CREATORS_TO_CHALLENGE
+    }
+
+    print(
+        f"OFVChallenger accuracy: {ofv_challenger_accuracy.accuracy*100:.2f}% out of {ofv_challenger_accuracy.total}"
+    )
+    for idx, accuracy_report in enumerate(olas_accuracies.values()):
+        if accuracy_report.total:
+            print(
+                f"Olas {idx} accuracy: {accuracy_report.accuracy*100:.2f}% out of {accuracy_report.total}"
+            )
+        else:
+            print(f"Olas {idx} accuracy: no answers found")
+
+
+if __name__ == "__main__":
+    main()
