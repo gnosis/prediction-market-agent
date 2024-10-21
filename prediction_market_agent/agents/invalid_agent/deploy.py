5f86c1b21b0e48acbd082cb34f52141f94e5e169
Peter Jung <peter@jung.ninja>
2024-10-21 11:59:39 +0200

    Add InvalidAgent

---
+++ prediction_market_agent/agents/invalid_agent/deploy.py
@@ -0,0 +1,40 @@
+from prediction_market_agent_tooling.deploy.agent import DeployableTraderAgent
+from prediction_market_agent_tooling.deploy.betting_strategy import (
+    BettingStrategy,
+    KellyBettingStrategy,
+)
+from prediction_market_agent_tooling.gtypes import Probability
+from prediction_market_agent_tooling.markets.agent_market import AgentMarket
+from prediction_market_agent_tooling.markets.data_models import ProbabilisticAnswer
+from prediction_market_agent_tooling.markets.markets import MarketType
+from prediction_market_agent_tooling.tools.is_invalid import is_invalid
+
+
+class InvalidAgent(DeployableTraderAgent):
+    bet_on_n_markets_per_run: int = 10
+
+    def verify_market(self, market_type: MarketType, market: AgentMarket) -> bool:
+        if market_type != MarketType.OMEN:
+            raise RuntimeError("This agent works only on Omen markets.")
+
+        if self.have_bet_on_market_since(market, since=self.same_market_bet_interval):
+            return False
+
+        # If the market is new, don't bet on it as the potential profit from market invalidity is low.
+        if 0.45 <= market.current_p_yes <= 0.55:
+            return False
+
+        # In contrast to the parent implementation, this agent will place bets only on invalid markets,
+        # it doesn't care whetever the market is predictable or not.
+        return is_invalid(market.question)
+
+    def get_betting_strategy(self, market: AgentMarket) -> BettingStrategy:
+        # Keep Kelly here! See `answer_binary_market`.
+        return KellyBettingStrategy(max_bet_amount=5)
+
+    def answer_binary_market(self, market: AgentMarket) -> ProbabilisticAnswer | None:
+        return ProbabilisticAnswer(
+            confidence=1.0,
+            p_yes=Probability(0.5),
+            reasoning="This market has been assessed as invalid, thus a probability of 0.5 is predicted. The Kelly strategy will opt for the less expensive outcome, adjusting the probability to 0.5, which is anticipated to be the final resolution where all outcome tokens hold equal value.",
+        )
