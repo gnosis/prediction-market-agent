deac15fb837b264ab12dcbd2b146c52a57435589
Peter Jung <peter@jung.ninja>
2024-12-04 13:40:20 +0100

    Sending messages from general agent

--- prediction_market_agent/agents/microchain_agent/messages_functions.py
+++ prediction_market_agent/agents/microchain_agent/messages_functions.py
@@ -1,28 +1,62 @@
 from microchain import Function
+from prediction_market_agent_tooling.gtypes import xdai_type
+from prediction_market_agent_tooling.tools.contract import ContractOnGnosisChain
+from prediction_market_agent_tooling.tools.web3_utils import send_xdai_to, xdai_to_wei
+from web3 import Web3
+
+from prediction_market_agent.agents.microchain_agent.microchain_agent_keys import (
+    MicrochainAgentKeys,
+)
+from prediction_market_agent.agents.microchain_agent.utils import compress_message
 
 
-class SendMessage(Function):
+class BroadcastPublicMessageToHumans(Function):
     @property
     def description(self) -> str:
-        return f"Use {SendMessage.__class__} to send a message to everyone."
+        return f"""Use {BroadcastPublicMessageToHumans.__name__} to send a message that humans can see. Use this to communicate with users that send you messages."""
 
     @property
     def example_args(self) -> list[str]:
         return ["Hello!"]
 
+    def __call__(self, message: str) -> str:
+        # TODO: Implement as needed in https://github.com/gnosis/prediction-market-agent/issues/570.
+        print(message)
+        return f"Message broadcasted to humans."
+
+
+class SendPaidMessageToAnotherAgent(Function):
+    @property
+    def description(self) -> str:
+        return f"""Use {SendPaidMessageToAnotherAgent.__name__} to send a message to an another agent, given his wallet address. 
+You can also specify the fee for the message, which will be deducted from your account. Higher the fee, bigger the chance that agent will read the message and act accordingly."""
+
+    @property
+    def example_args(self) -> list[str]:
+        return ["0x123", "Hello!", "0.001"]
+
     def __call__(
         self,
+        address: str,
         message: str,
+        fee: float,
     ) -> str:
-        # TODO: Complete the logic.
-        return "Message sent."
+        keys = MicrochainAgentKeys()
+        send_xdai_to(
+            web3=ContractOnGnosisChain.get_web3(),
+            from_private_key=keys.bet_from_private_key,
+            to_address=Web3.to_checksum_address(address),
+            value=xdai_to_wei(keys.cap_sending_xdai(xdai_type(fee))),
+            data_text=compress_message(message),
+        )
+        return "Message sent to the agent."
 
 
 class ReceiveMessage(Function):
     @property
     def description(self) -> str:
         # TODO: Add number of unseen messages to the description.
-        return f"Use {ReceiveMessage.__class__} to receive last unseen message from the users."
+        return f"Use {ReceiveMessage.__name__} to receive last unseen message from the users."
 
     @property
     def example_args(self) -> list[str]:
@@ -36,6 +70,7 @@ class ReceiveMessage(Function):
 
 
 MESSAGES_FUNCTIONS: list[type[Function]] = [
-    SendMessage,
+    BroadcastPublicMessageToHumans,
+    SendPaidMessageToAnotherAgent,
     ReceiveMessage,
 ]
