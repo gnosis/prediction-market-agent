c38d21d8b3bb0dceab1bd382c75a50f2efb7287a
Gabriel Fior <fior.gbm@gmail.com>
2024-12-06 13:05:17 -0300

    Make blockchain agent execute transactions on-chain (#566)
    * Agent executed successfully
    
    * Agent able to read USDC balance and ConditionalTokens balance
    
    * Refactored code - able to read balance from a given tokenID onchain successfully
    
    * Small changes before PR
    
    * Shelving changes
    
    * Update prediction_market_agent/agents/learnable_agent/app.py
    
    Co-authored-by: Peter Jung <peter@jung.ninja>
    
    * Update prediction_market_agent/agents/learnable_agent/app.py
    
    Co-authored-by: Peter Jung <peter@jung.ninja>
    
    * PR changes
    
    * Updated poetry.lock
    
    * Fixed mypy
    
    * WIP
    
    * Run script working for fetching conditional balances
    
    * WIP
    
    * WIP
    
    * WIP (2)
    
    * Refactoring for new Autogen version
    
    * Fixing mypy
    
    * Last fixes before PR review
    
    * Apply suggestions from code review
    
    Co-authored-by: Peter Jung <peter@jung.ninja>
    
    * Update prediction_market_agent/agents/blockchain_coding_agent/app.py
    
    Co-authored-by: Peter Jung <peter@jung.ninja>
    
    * Update prediction_market_agent/agents/blockchain_coding_agent/app.py
    
    Co-authored-by: Peter Jung <peter@jung.ninja>
    
    * Implemented PR comments
    
    * Further PR comments addressed
    
    * Avoiding infinite recursion when fetching smart contract code
    
    * Updated poetry.lock
    
    * Fixed CI
    
    * Updated poetry.lock after merge
    
    ---------
    
    Co-authored-by: Peter Jung <peter@jung.ninja>

---
+++ scripts/blockchain_agent/run_agent04_read.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from autogen_agentchat.task import Console
+
+from prediction_market_agent.agents.blockchain_coding_agent.agents import get_agent_team
+from prediction_market_agent.agents.blockchain_coding_agent.prompts import (
+    CALL_ONCHAIN_FUNCTION_PROMPT,
+)
+
+
+async def main() -> None:
+    agent_team = get_agent_team()
+    stream = agent_team.run_stream(task=CALL_ONCHAIN_FUNCTION_PROMPT)
+    await Console(stream)
+
+
+asyncio.run(main())
