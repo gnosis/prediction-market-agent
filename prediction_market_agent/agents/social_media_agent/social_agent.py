a80031b516b4a0f7ade7aadf967a0facb691564d
gabrielfior <fior.gbm@gmail.com>
2024-12-10 17:39:12 -0300

    Code compiling, tweet still not great

--- prediction_market_agent/agents/social_media_agent/social_agent.py
+++ prediction_market_agent/agents/social_media_agent/social_agent.py
@@ -1,10 +1,13 @@
+import asyncio
 from enum import Enum
 from string import Template
 from typing import Any, Dict, Optional
 
-import autogen
-from autogen import AssistantAgent, UserProxyAgent
-from autogen.cache import Cache
+from autogen_agentchat.agents import AssistantAgent, UserProxyAgent
+from autogen_agentchat.conditions import MaxMessageTermination, TextMentionTermination
+from autogen_agentchat.teams import BaseGroupChat, RoundRobinGroupChat
+from autogen_agentchat.ui import Console
+from autogen_ext.models.openai import OpenAIChatCompletionClient
 from prediction_market_agent_tooling.markets.data_models import Bet
 from prediction_market_agent_tooling.tools.langfuse_ import observe
 from prediction_market_agent_tooling.tools.utils import DatetimeUTC
@@ -16,8 +19,8 @@ from prediction_market_agent.agents.microchain_agent.memory import (
 from prediction_market_agent.agents.social_media_agent.prompts import (
     CRITIC_PROMPT,
     INFLUENCER_PROMPT,
-    POST_MAX_LENGTH,
     REASONING_PROMPT,
+    POST_MAX_LENGTH,
 )
 from prediction_market_agent.agents.utils import extract_reasonings_to_learnings
 from prediction_market_agent.db.long_term_memory_table_handler import (
@@ -66,51 +69,67 @@ def reflection_message(
     return reflect_prompt
 
 
-def build_llm_config(model: str) -> Dict[str, Any]:
-    keys = APIKeys()
-    return {
-        "config_list": [
-            {"model": model, "api_key": keys.openai_api_key.get_secret_value()}
-        ],
-    }
+def build_team(model: str) -> BaseGroupChat:
+    # user_proxy = UserProxyAgent(
+    #     name="User",
+    #     human_input_mode="NEVER",
+    #     is_termination_msg=lambda x: x.get("content", "").find("TERMINATE") >= 0,
+    #     code_execution_config={
+    #         "last_n_messages": 1,
+    #         "work_dir": "tasks",
+    #         "use_docker": False,
+    #     },
+    # )
 
+    # writer = AssistantAgent(
+    #     name="Writer",
+    #     llm_config=llm_config,
+    #     system_message="""You are a professional influencer, known for your insightful and engaging tweets. You
+    #     transform complex concepts into compelling narratives. You should improve the quality of the content based on
+    #     the feedback from the user. You must always return only the tweet. """,
+    # )
 
-def build_agents(model: str) -> Dict[AutogenAgentType, autogen.ConversableAgent]:
-    llm_config = build_llm_config(model)
+    #     critic = AssistantAgent(
+    #         name="Critic",
+    #         llm_config=llm_config,
+    #         system_message=f""" You are a critic, known for your thoroughness and commitment to standards. Your task is
+    #         to scrutinize content for any harmful elements or regulatory violations, ensuring all materials align with
+    #         required guidelines. You should also always remind everyone that the limit for any posts being created is
+    # {POST_MAX_LENGTH} characters. References to betting and gambling are allowed.""",
+    #     )
 
-    user_proxy = autogen.UserProxyAgent(
-        name="User",
-        human_input_mode="NEVER",
-        is_termination_msg=lambda x: x.get("content", "").find("TERMINATE") >= 0,
-        code_execution_config={
-            "last_n_messages": 1,
-            "work_dir": "tasks",
-            "use_docker": False,
-        },
+    # Create an OpenAI model client.
+    model_client = OpenAIChatCompletionClient(
+        model=model, api_key=APIKeys().openai_api_key.get_secret_value()
     )
 
-    writer = autogen.AssistantAgent(
-        name="Writer",
-        llm_config=llm_config,
-        system_message="""You are a professional influencer, known for your insightful and engaging tweets. You 
-        transform complex concepts into compelling narratives. You should improve the quality of the content based on 
-        the feedback from the user. You must always return only the tweet. """,
+    # Create the primary agent.
+    primary_agent = AssistantAgent(
+        "primary",
+        model_client=model_client,
+        description="Generate a tweet for the user. Make sure the feedback given by the critic is being addressed.",
+        system_message="""You are a professional influencer, known for your insightful and engaging tweets. You transform complex concepts into compelling narratives. You should improve the quality of the content based on the feedback from the user. You must always return only the tweet.""",
     )
 
-    critic = autogen.AssistantAgent(
-        name="Critic",
-        llm_config=llm_config,
-        system_message=f""" You are a critic, known for your thoroughness and commitment to standards. Your task is 
-        to scrutinize content for any harmful elements or regulatory violations, ensuring all materials align with 
-        required guidelines. You should also always remind everyone that the limit for any posts being created is 
-{POST_MAX_LENGTH} characters. References to betting and gambling are allowed.""",
+    # Create the critic agent.
+    critic_agent = AssistantAgent(
+        "critic",
+        model_client=model_client,
+        description="Critique the tweet and provide feedback.",
+        system_message=f""" You are a critic, known for your thoroughness and commitment to standards. Your task is to scrutinize content for any harmful elements or regulatory violations, ensuring all materials align with required guidelines. You should also always remind everyone that the limit for any posts being created is {POST_MAX_LENGTH} characters. References to betting and gambling are allowed.""",
+        # Respond with 'APPROVE' to when your feedbacks are addressed.""",
     )
 
-    return {
-        AutogenAgentType.CRITIC: critic,
-        AutogenAgentType.WRITER: writer,
-        AutogenAgentType.USER: user_proxy,
-    }
+    text_termination = TextMentionTermination("APPROVE")
+    max_message_termination = MaxMessageTermination(5)
+    # Combine the termination conditions using the `|`` operator so that the
+    # task stops when either condition is met.
+    termination = text_termination | max_message_termination
+
+    reflection_team = RoundRobinGroupChat(
+        [primary_agent, critic_agent], termination_condition=termination, max_turns=4
+    )
+    return reflection_team
 
 
 @observe()
@@ -184,37 +203,27 @@ def build_reply_tweet(
 
 
 def build_tweet(model: str, task: str) -> str:
-    agents = build_agents(model)
-    user_proxy, writer, critic = (
-        agents[AutogenAgentType.USER],
-        agents[AutogenAgentType.WRITER],
-        agents[AutogenAgentType.CRITIC],
-    )
-    user_proxy.register_nested_chats(
-        [
-            {
-                "recipient": critic,
-                "message": reflection_message,
-                "summary_method": "last_msg",
-                "max_turns": 1,
-            }
-        ],
-        trigger=writer,
-    )
+    team = build_team(model)
+
+    # ToDo - prompt better so it always produces a tweet
+    # ToDo - Follow this https://microsoft.github.io/autogen/dev/user-guide/agentchat-user-guide/tutorial/teams.html#team-usage-guide
+
+    task_result = asyncio.run(Console(team.run_stream(task=task)))
+    reply_tweet = task_result.messages[-2].content  # Last message is critic's approval
 
     # in case we trigger repeated runs, Cache makes it faster.
-    with Cache.disk(cache_seed=42) as cache:
-        # max_turns = the maximum number of turns for the chat between the two agents. One turn means one conversation round trip.
-        res = user_proxy.initiate_chat(
-            recipient=writer,
-            message=task,
-            max_turns=2,
-            summary_method=SummaryMethod.REFLECTION_WITH_LLM,
-            cache=cache,
-        )
+    # with Cache.disk(cache_seed=42) as cache:
+    #     # max_turns = the maximum number of turns for the chat between the two agents. One turn means one conversation round trip.
+    #     res = user_proxy.initiate_chat(
+    #         recipient=writer,
+    #         message=task,
+    #         max_turns=2,
+    #         summary_method=SummaryMethod.REFLECTION_WITH_LLM,
+    #         cache=cache,
+    #     )
     # We extract the last message since the revised tweet is contained in the last response
     # from the writer.
-    reply_tweet = res.chat_history[-1]["content"]
+    # reply_tweet = res.chat_history[-1]["content"]
 
     return str(
         reply_tweet
