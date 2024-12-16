0c13ce2411ebdae1a3412f276c8b5e0cbad1b513
Peter Jung <peter@jung.ninja>
2024-12-16 20:39:18 +0100

    fix tests

--- tests/agents/microchain/test_messages_functions.py
+++ tests/agents/microchain/test_messages_functions.py
@@ -80,7 +80,7 @@ def patch_spice() -> Generator[PropertyMock, None, None]:
 def patch_send_xdai() -> Generator[PropertyMock, None, None]:
     # Note that we patch where the function is called (see https://docs.python.org/3/library/unittest.mock.html#where-to-patch).
     with patch(
-        "prediction_market_agent.agents.microchain_agent.messages_functions.send_xdai_to",
+        "prediction_market_agent.agents.microchain_agent.nft_treasury_game.messages_functions.send_xdai_to",
         return_value={"transactionHash": HexBytes(MOCK_HASH_1)},
     ) as mock_send_xdai:
         yield mock_send_xdai
