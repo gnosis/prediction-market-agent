6fb17ed976f5bb1871c9d5980e5530b21ca022e0
Peter Jung <peter@jung.ninja>
2024-12-16 11:29:39 +0100

    fix maybe

--- prediction_market_agent/agents/microchain_agent/balance_functions.py
+++ prediction_market_agent/agents/microchain_agent/balance_functions.py
@@ -1,6 +1,6 @@
 from microchain import Function
-from prediction_market_agent_tooling.gtypes import ChecksumAddress
 from prediction_market_agent_tooling.tools.balances import get_balances
+from web3 import Web3
 
 from prediction_market_agent.utils import APIKeys
 
@@ -14,8 +14,8 @@ class GetOtherWalletXDAIBalance(Function):
     def example_args(self) -> list[str]:
         return ["0xSomeAddress"]
 
-    def __call__(self, wallet_address: ChecksumAddress) -> str:
-        balances = get_balances(wallet_address)
+    def __call__(self, wallet_address: str) -> str:
+        balances = get_balances(Web3.to_checksum_address(wallet_address))
         return f"Balance of {wallet_address} is {balances.xdai} xDai and {balances.wxdai} wxDai."
 
 
