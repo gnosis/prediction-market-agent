1da04641df444efcbf10ad485b8300b6ac20fa9a
Peter Jung <peter@jung.ninja>
2025-03-26 15:47:39 +0100

    Add info to sleep message

--- prediction_market_agent/agents/microchain_agent/nft_treasury_game/nft_game_messages_functions.py
+++ prediction_market_agent/agents/microchain_agent/nft_treasury_game/nft_game_messages_functions.py
@@ -157,7 +157,7 @@ class SleepUntil(Function):
     Therefore, the logic itself is implemented in `execute_calling_of_this_function` and used in the iteration callback of the agent.
     """
 
-    OK_OUTPUT = "Sleeping. Don't forget to check the status of the game and your messages once you wake up!"
+    OK_OUTPUT = "Sleeping. Don't forget to check the status of the game, your NFT keys balance and your messages once you wake up!"
     SLEEP_THRESHOLD = timedelta(hours=1)
 
     def __init__(self) -> None:
