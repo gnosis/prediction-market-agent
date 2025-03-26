f40693450dd59b02442ac9d1a0a7e6c205e57316
gabrielfior <fior.gbm@gmail.com>
2025-03-26 19:03:50 -0300

    reverted coinflip

--- prediction_market_agent/agents/coinflip_agent/deploy.py
+++ prediction_market_agent/agents/coinflip_agent/deploy.py
@@ -2,18 +2,12 @@ import random
 
 from prediction_market_agent_tooling.deploy.agent import DeployableTraderAgent
 from prediction_market_agent_tooling.gtypes import Probability
-from prediction_market_agent_tooling.markets.agent_market import AgentMarket, SortBy
+from prediction_market_agent_tooling.markets.agent_market import AgentMarket
 from prediction_market_agent_tooling.markets.data_models import ProbabilisticAnswer
 from prediction_market_agent_tooling.markets.markets import MarketType
 
 
 class DeployableCoinFlipAgent(DeployableTraderAgent):
-    n_markets_to_fetch = 5
-    get_markets_sort_by = SortBy.HIGHEST_LIQUIDITY
-    bet_on_n_markets_per_run = 1
-
-    # ToDo - Avoid markets where we placed bets previously
-
     def verify_market(self, market_type: MarketType, market: AgentMarket) -> bool:
         return True
 
