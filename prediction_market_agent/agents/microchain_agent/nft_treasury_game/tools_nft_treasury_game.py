5d29a2cb0d47e222fe350d3d41f30bf819396fdf
Peter Jung <peter@jung.ninja>
2025-03-28 10:24:18 +0100

    lower start date

--- prediction_market_agent/agents/microchain_agent/nft_treasury_game/tools_nft_treasury_game.py
+++ prediction_market_agent/agents/microchain_agent/nft_treasury_game/tools_nft_treasury_game.py
@@ -53,7 +53,7 @@ def get_end_datetime_of_previous_round() -> DatetimeUTC | None:
 
 def get_start_time_of_current_round() -> DatetimeUTC | None:
     # TODO: Dynamically from somewhere and they must be updated in the correct order/timing.
-    return DatetimeUTC(year=2025, month=3, day=28, hour=10, minute=0, second=0)
+    return DatetimeUTC(year=2025, month=3, day=28, hour=9, minute=0, second=0)
 
 
 def get_end_datetime_of_current_round() -> DatetimeUTC:
