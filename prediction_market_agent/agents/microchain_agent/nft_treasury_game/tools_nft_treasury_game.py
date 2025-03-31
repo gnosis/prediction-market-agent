7367d5aaa9a900d5429149f1dd4231ca25d66955
Peter Jung <peter@jung.ninja>
2025-03-31 11:17:18 +0200

    Next round

--- prediction_market_agent/agents/microchain_agent/nft_treasury_game/tools_nft_treasury_game.py
+++ prediction_market_agent/agents/microchain_agent/nft_treasury_game/tools_nft_treasury_game.py
@@ -43,17 +43,17 @@ def get_nft_game_is_finished(web3: Web3 | None = None) -> bool:
 
 def get_start_time_of_previous_round() -> DatetimeUTC | None:
     # TODO: Dynamically from somewhere and they must be updated in the correct order/timing.
-    return DatetimeUTC(year=2025, month=3, day=31, hour=6, minute=0, second=0)
+    return DatetimeUTC(year=2025, month=3, day=31, hour=8, minute=15, second=0)
 
 
 def get_end_datetime_of_previous_round() -> DatetimeUTC | None:
     # TODO: Dynamically from somewhere and they must be updated in the correct order/timing.
-    return DatetimeUTC(year=2025, month=3, day=31, hour=8, minute=0, second=0)
+    return DatetimeUTC(year=2025, month=3, day=31, hour=9, minute=5, second=0)
 
 
 def get_start_time_of_current_round() -> DatetimeUTC | None:
     # TODO: Dynamically from somewhere and they must be updated in the correct order/timing.
-    return DatetimeUTC(year=2025, month=3, day=31, hour=8, minute=15, second=0)
+    return DatetimeUTC(year=2025, month=3, day=31, hour=9, minute=15, second=0)
 
 
 def get_end_datetime_of_current_round() -> DatetimeUTC:
