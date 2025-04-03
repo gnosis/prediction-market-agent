9a91a644ffd72cad6366ff199972e3ee848e0e3d
Gabriel Fior <fior.gbm@gmail.com>
2025-04-03 12:13:15 -0300

    Added Coinflip seer agent (#773)
    * Added Coinflip seer agent
    
    * reverted coinflip
    
    * Added to run_agent.py
    
    * Bumped PMAT
    
    * Updated PMAT

--- prediction_market_agent/agents/coinflip_agent/deploy.py
+++ prediction_market_agent/agents/coinflip_agent/deploy.py
@@ -1,8 +1,13 @@
 import random
+from datetime import timedelta
 
 from prediction_market_agent_tooling.deploy.agent import DeployableTraderAgent
+from prediction_market_agent_tooling.deploy.trade_interval import (
+    FixedInterval,
+    TradeInterval,
+)
 from prediction_market_agent_tooling.gtypes import Probability
-from prediction_market_agent_tooling.markets.agent_market import AgentMarket
+from prediction_market_agent_tooling.markets.agent_market import AgentMarket, SortBy
 from prediction_market_agent_tooling.markets.data_models import ProbabilisticAnswer
 from prediction_market_agent_tooling.markets.markets import MarketType
 
@@ -18,3 +23,10 @@ class DeployableCoinFlipAgent(DeployableTraderAgent):
             p_yes=Probability(float(decision)),
             reasoning="I flipped a coin to decide.",
         )
+
+
+class DeployableCoinFlipAgentByHighestLiquidity(DeployableCoinFlipAgent):
+    n_markets_to_fetch = 10
+    get_markets_sort_by = SortBy.HIGHEST_LIQUIDITY
+    bet_on_n_markets_per_run = 2
+    same_market_trade_interval: TradeInterval = FixedInterval(timedelta(days=14))
