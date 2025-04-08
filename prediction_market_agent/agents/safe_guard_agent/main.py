1ee7e0fac7f6932b6c72fe5f18f0308fbc1fde1d
Peter Jung <peter@jung.ninja>
2025-04-08 13:47:14 +0200

    Run Safe Guard in loop to provide faster validations (#790)

--- prediction_market_agent/agents/safe_guard_agent/main.py
+++ prediction_market_agent/agents/safe_guard_agent/main.py
@@ -1,6 +1,9 @@
+import time
+
 import typer
 from prediction_market_agent_tooling.config import APIKeys
 from prediction_market_agent_tooling.deploy.agent import initialize_langfuse
+from prediction_market_agent_tooling.loggers import logger
 
 from prediction_market_agent.agents.safe_guard_agent.safe_guard import validate_all
 
@@ -11,13 +14,22 @@ def main(
     ),
     do_reject: bool = typer.Option(False, help="Reject transaction if not validated"),
     do_message: bool = typer.Option(False, help="Send a message about the outcome"),
+    run_time_limit: int = 55 * 60,  # seconds
+    sleep_between_validations: int = 15,  # seconds
 ) -> None:
     initialize_langfuse(enable_langfuse=APIKeys().default_enable_langfuse)
-    validate_all(
-        do_sign_or_execution=do_sign_or_execution,
-        do_reject=do_reject,
-        do_message=do_message,
-    )
+
+    start_time = time.time()
+    while time.time() - start_time < run_time_limit:
+        validate_all(
+            do_sign_or_execution=do_sign_or_execution,
+            do_reject=do_reject,
+            do_message=do_message,
+        )
+        logger.info(
+            f"Waiting for {sleep_between_validations} seconds before next validation..."
+        )
+        time.sleep(sleep_between_validations)
 
 
 if __name__ == "__main__":
