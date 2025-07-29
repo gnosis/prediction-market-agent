d7321ea822d8719935279df3a89ba3cbf07aebdc
gabrielfior <fior.gbm@gmail.com>
2025-07-29 18:25:05 -0300

    deps ok

--- prediction_market_agent/agents/coinflip_agent/deploy.py
+++ prediction_market_agent/agents/coinflip_agent/deploy.py
@@ -1,18 +1,18 @@
 import random
-from datetime import timedelta
 
 from prediction_market_agent_tooling.deploy.agent import DeployableTraderAgent
-from prediction_market_agent_tooling.deploy.trade_interval import (
-    FixedInterval,
-    TradeInterval,
-)
 from prediction_market_agent_tooling.gtypes import Probability
 from prediction_market_agent_tooling.markets.agent_market import AgentMarket, SortBy
-from prediction_market_agent_tooling.markets.data_models import ProbabilisticAnswer
+from prediction_market_agent_tooling.markets.data_models import (
+    ProbabilisticAnswer,
+    ScalarProbabilisticAnswer,
+)
 from prediction_market_agent_tooling.markets.markets import MarketType
 
 
 class DeployableCoinFlipAgent(DeployableTraderAgent):
+    n_markets_to_fetch = 10
+
     def verify_market(self, market_type: MarketType, market: AgentMarket) -> bool:
         return True
 
@@ -27,5 +27,17 @@ class DeployableCoinFlipAgent(DeployableTraderAgent):
 
 class DeployableCoinFlipAgentByHighestLiquidity(DeployableCoinFlipAgent):
     get_markets_sort_by = SortBy.HIGHEST_LIQUIDITY
-    bet_on_n_markets_per_run = 2
-    same_market_trade_interval: TradeInterval = FixedInterval(timedelta(days=14))
+    bet_on_n_markets_per_run = 1
+    n_markets_to_fetch = 2
+    # same_market_trade_interval: TradeInterval = FixedInterval(timedelta(days=14))
+
+    def answer_scalar_market(
+        self, market: AgentMarket
+    ) -> ScalarProbabilisticAnswer | None:
+        return ScalarProbabilisticAnswer(
+            confidence=0.5,
+            upperBound=market.upper_bound,
+            lowerBound=market.lower_bound,
+            scalar_value=(market.upper_bound + market.lower_bound) / 2,
+            reasoning="I flipped a coin to decide.",
+        )
