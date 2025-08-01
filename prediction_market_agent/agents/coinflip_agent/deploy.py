90a5dcaf4177c6a4a3e3a372ff69deefd3ecf55c
gabrielfior <fior.gbm@gmail.com>
2025-08-01 19:25:49 -0300

    buy tokens working

--- prediction_market_agent/agents/coinflip_agent/deploy.py
+++ prediction_market_agent/agents/coinflip_agent/deploy.py
@@ -1,6 +1,10 @@
 import random
 
 from prediction_market_agent_tooling.deploy.agent import DeployableTraderAgent
+from prediction_market_agent_tooling.deploy.betting_strategy import (
+    BettingStrategy,
+    MaxExpectedValueBettingStrategy,
+)
 from prediction_market_agent_tooling.gtypes import Probability
 from prediction_market_agent_tooling.markets.agent_market import AgentMarket, SortBy
 from prediction_market_agent_tooling.markets.data_models import (
@@ -12,6 +16,8 @@ from prediction_market_agent_tooling.markets.markets import MarketType
 
 class DeployableCoinFlipAgent(DeployableTraderAgent):
     n_markets_to_fetch = 10
+    bet_on_n_markets_per_run = 4
+    get_markets_sort_by: SortBy = SortBy.CLOSING_SOONEST
 
     def verify_market(self, market_type: MarketType, market: AgentMarket) -> bool:
         return True
@@ -22,6 +28,13 @@ class DeployableCoinFlipAgent(DeployableTraderAgent):
             confidence=0.5,
             p_yes=Probability(float(decision)),
             reasoning="I flipped a coin to decide.",
+        )
+
+    def get_betting_strategy(self, market: AgentMarket) -> BettingStrategy:
+        # ToDo - remove me
+        total_amount = self.get_total_amount_to_bet(market)
+        return MaxExpectedValueBettingStrategy(
+            max_position_amount=total_amount, take_profit=False
         )
 
 
