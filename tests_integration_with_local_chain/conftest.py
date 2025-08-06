344ac39eaf1131679891a1bc7d7f1222f65ac85a
Peter Jung <peter@jung.ninja>
2025-08-06 12:14:24 +0200

    Unified way with PMAT

--- tests_integration_with_local_chain/conftest.py
+++ tests_integration_with_local_chain/conftest.py
@@ -35,7 +35,7 @@ def load_env() -> None:
 
 @pytest.fixture(scope="session")
 def local_web3(
-    load_env: None, chain: ChainManager, accounts: list[TestAccount]
+    load_env: None, chain: ChainManager, eoa_accounts: list[TestAccount]
 ) -> t.Generator[Web3, None, None]:
     print("entering fixture local_web3")
 
@@ -44,7 +44,9 @@ def local_web3(
         w3 = Web3(Web3.HTTPProvider(tenderly_fork_rpc))
         print("funding test accounts on tenderly")
         fund_account_on_tenderly(
-            tenderly_fork_rpc, [a.address for a in accounts], xDai(1000)
+            tenderly_fork_rpc,
+            [a.address for a in eoa_accounts],
+            xDai(1000),
         )
         set_env_rpc_url(tenderly_fork_rpc)
         yield w3
@@ -60,34 +62,28 @@ def local_web3(
     print("exiting fixture local_web3")
 
 
+@pytest.fixture(scope="session")
+def eoa_accounts() -> list[TestAccount]:
+    # Note that the test accounts we use here are EOAs (because we use a different mnemonic).
+    # If we were using anvil standard accounts, we would have to filter out smart accounts.
+    # For example, anvil's acc 1 (https://gnosisscan.io/address/0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266) delegated access to a smart contract, which sweeps all funds sent to it.
+    # Hence we prefer to use EOA accounts from a different mnemonic than to rely on anvil's standard accounts.
+    return ape_accounts.test_accounts
+
+
 @pytest.fixture(scope="module")
 def local_ethereum_client(local_web3: Web3) -> EthereumClient:
     return EthereumClient(URI(local_web3.provider.endpoint_uri))  # type: ignore
 
 
 @pytest.fixture(scope="session")
-def many_test_keys(local_web3: Web3) -> list[APIKeys]:
-    # Create fresh EOA accounts and fund them from one of the ape's testing accounts.
-    fund_from_account = ape_accounts.test_accounts[0]
+def test_keys(eoa_accounts: list[TestAccount]) -> APIKeys:
+    account = eoa_accounts[0]
 
-    accounts = [
-        create_and_fund_random_account(
-            local_web3, private_key_type(fund_from_account.private_key)
-        )
-        for _ in range(10)
-    ]
-
-    return [
-        APIKeys(
-            BET_FROM_PRIVATE_KEY=private_key_type(account.key.hex()), SAFE_ADDRESS=None
-        )
-        for account in accounts
-    ]
-
-
-@pytest.fixture(scope="session")
-def test_keys(many_test_keys: list[APIKeys]) -> APIKeys:
-    return many_test_keys[0]
+    # Using a standard Anvil account with enough xDAI.
+    return APIKeys(
+        BET_FROM_PRIVATE_KEY=private_key_type(account.private_key), SAFE_ADDRESS=None
+    )
 
 
 def fund_account_on_tenderly(
